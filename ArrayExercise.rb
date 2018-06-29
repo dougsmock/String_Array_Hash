c1b5c48007741b735e345477703f1ce32f0d471e
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-29 16:08:50 -0400

    Add files via upload

--- ArrayExercise.rb
+++ ArrayExercise.rb
@@ -1,7 +1,7 @@
 slice_this_arr = ["1", "2", "3"]
 
 def my_slice(slice_this_arr)
-  slice_this_arr.delete(1)
+  slice_this_arr.delete_at(1)
   slice_this_arr
 end
 
@@ -10,15 +10,6 @@ end
 
 
 
-
-
-
-
-
-# delete_at(index) → obj or nil
-#
-#
-#
 #
 # a = ["ant", "bat", "cat", "dog"]
 # a.delete_at(2)    #=> "cat"
