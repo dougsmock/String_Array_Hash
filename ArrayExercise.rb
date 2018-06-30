32cf800a9d56355827ccdcad61bcaec0beb5bfef
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-30 15:50:03 -0400

    Add files via upload

--- ArrayExercise.rb
+++ ArrayExercise.rb
@@ -1,9 +1,25 @@
-slice_this_arr = ["1", "2", "3"]
-
-def my_slice(slice_this_arr)
-  slice_this_arr.delete_at(1)
-  slice_this_arr
+slice_arr = ["1", "2", "3"]
+def my_slice(slice_arr)
+  slice_spot = 0
+  if (slice_spot + 1) > slice_arr.length
+    nil
+  elsif (slice_spot + 1) <= slice_arr.length
+    chopped = slice_arr.delete_at(slice_spot)
+    p chopped
+    p slice_arr
+  end
 end
+
+
+
+
+#if slice number + 1 > slice length, nil, array stays same
+#if slice number + 1 <= slice length,
+#state position erased, state rest of array
+
+
+
+
 
 
 
