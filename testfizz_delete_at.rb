fc8588cacd1b27068613a7c919053d6a5ca264ad
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-30 16:10:09 -0400

    Add files via upload

--- testfizz_delete_at.rb
+++ testfizz_delete_at.rb
@@ -3,14 +3,16 @@ require_relative "ArrayExercise.rb"
 
 class DeleteAt < Minitest::Test
 
-  def test_assert_that_1_equals_1
-    assert_equal(1, 1)
-  end
+  # def test_assert_that_1_equals_1
+  #   assert_equal(1, 1)
+  # end
 
   def test_if_delete_at_equals
     slice_arr = ["1", "2", "3"]
-    assert_equal(slice_arr[0]) == "1"
-    assert_equal(slice_arr(1)) == "2"
-    assert_equal(slice_arr(2)) == "3"
+    assert_equal(slice_arr[0], "1")
+    assert_equal(slice_arr[1], "2")
+    assert_equal(slice_arr[3], "4")
+
   end
+
 end
