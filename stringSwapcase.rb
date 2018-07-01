f7ceb83605dd6f47225908ceab6dc5bd0bd5e9d9
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-07-01 10:20:54 -0400

    Add files via upload

---
+++ stringSwapcase.rb
@@ -0,0 +1,21 @@
+# "BellyAching".swapcase
+
+def change_case()
+  string = []
+  string.split('')
+  i = 0
+  if string[i] = string[i].upcase
+    string[i].downcase
+  elsif string[i] = string[i].downcase
+    string[i].upcase
+  end
+  
+
+
+
+  end
+
+
+
+
+end
