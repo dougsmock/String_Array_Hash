b82a357909b0c067f34a92306ea1697cf8a76ec5
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-07-01 10:31:42 -0400

    Add files via upload

--- stringSwapcase.rb
+++ stringSwapcase.rb
@@ -3,11 +3,13 @@
 def change_case()
   string = []
   i = 0
-  if string[i] = string[i].upcase
-    string[i].downcase
-    i += 1
-  elsif string[i] = string[i].downcase
-    string[i].upcase
-    i += 1
+  while i <= string.length
+    if string[i] = string[i].upcase
+      string[i].downcase
+      i += 1
+    elsif string[i] = string[i].downcase
+      string[i].upcase
+      i += 1
+    end
   end
 end
